8ca5d83e399e02a48238d63ff8a37370776e754d
karina <karina.m@live.ca>
2018-02-16 15:41:53 -0500

    Finish SaveFolder method

--- ImageManipulation/ImageManipulation/ImageUtilities.cs
+++ ImageManipulation/ImageManipulation/ImageUtilities.cs
@@ -14,15 +14,6 @@ namespace ImageManipulation
 
         /// <summary>
         /// Loads images from folder
-        /// 
-        /// Write a method LoadFolder which takes as input a String representing 
-        /// a directory path and returns an Image[]. This method should look at 
-        /// all the ﬁles with the extension pnm or pgm inside a directory 
-        /// (Hint: use the library method Directory.GetFiles) and create an Image[]
-        /// based on them, using the serializers you created above, choosing the 
-        /// correct one to use based on the extension of the ﬁle. To do the actual
-        /// reading, you should use the StreamReader class.
-        /// 
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
@@ -52,15 +43,7 @@ namespace ImageManipulation
         }
 
         /// <summary>
-        /// 
         /// Saves images to folder
-        /// 
-        /// Write a method SaveFolder which takes as input a String representing a 
-        /// directory path as well as an Image[] and a String format.This method should
-        /// export every Image in the array to the folder given using the speciﬁed 
-        /// format(assumed to be “pnm” or “pgm”). To do the actual writing, you should
-        /// use the StreamWriter class.
-        ///
         /// </summary>
         /// <param name="images"></param>
         /// <param name="path"></param>
@@ -80,16 +63,16 @@ namespace ImageManipulation
         private void SaveFormat(Image[] images, String path, IImageSerialization format, String ext)
         {
             int filenum = 1;
-            String filename = "image" + filenum + "." + ext;
+            String filename = path + "image" + filenum + "." + ext;
             foreach (Image img in images)
             {
-                using (StreamWriter str = new StreamWriter(new FileStream(path, FileMode.Create, FileAccess.Write)))
+                using (StreamWriter str = new StreamWriter(new FileStream(filename, FileMode.Create, FileAccess.Write)))
 
                 {
                     String data = format.Serialize(img);
+                    str.WriteLine(data);
                 }
-            }                
-            
+            } 
         }
     }
 }
