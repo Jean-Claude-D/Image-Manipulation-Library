721c8a9a3cab27cb0044e1b5e2695de326123de3
karina <karina.m@live.ca>
2018-02-16 16:04:37 -0500

    Add more crop tests

--- ImageManipulation/ImageTests/ImageTest.cs
+++ ImageManipulation/ImageTests/ImageTest.cs
@@ -105,7 +105,7 @@ namespace ImageTests
 
             Pixel[,] pixel = { { new Pixel(122), new Pixel(170) },
                                  { new Pixel(185), new Pixel(124)} };
-            
+
             Image expected = new Image("ey", 255, pixel);
             Assert.AreEqual(expected, result);
         }
@@ -142,7 +142,7 @@ namespace ImageTests
                 }
             }
             Image result = new Image("ey", 225, pixels);
-            result.Crop(0,0,5,5);
+            result.Crop(0, 0, 5, 5);
 
             Pixel[,] pixel = new Pixel[5, 5];
             for (int i = 0; i < pixel.GetLength(0); i++)
@@ -200,6 +200,42 @@ namespace ImageTests
 
             Image expected = new Image("ey", 255, pixels);
             Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ImageCropInvalidInputTest()
+        {
+            Pixel[,] pixel = {  { new Pixel(124), new Pixel(185), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(222), new Pixel(115) },
+                                { new Pixel(123), new Pixel(167), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(124), new Pixel(185) },
+                                { new Pixel(124), new Pixel(185), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(222), new Pixel(115) },
+                                { new Pixel(123), new Pixel(167), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(124), new Pixel(185) } };
+
+            Image result = new Image("ey", 255, pixel);            
+
+            Action action = delegate { result.Crop(0, 6, 0, 0); };
+            Assert.ThrowsException<ArgumentException>(action);
+        }
+
+        [TestMethod]
+        public void ImageCropInvalidInput2Test()
+        {
+            Pixel[,] pixel = {  { new Pixel(124), new Pixel(185), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(222), new Pixel(115) },
+                                { new Pixel(123), new Pixel(167), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(124), new Pixel(185) },
+                                { new Pixel(124), new Pixel(185), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(222), new Pixel(115) },
+                                { new Pixel(123), new Pixel(167), new Pixel(170), new Pixel(122) },
+                                { new Pixel(170), new Pixel(122), new Pixel(124), new Pixel(185) } };
+
+            Image result = new Image("ey", 255, pixel);
+
+            Action action = delegate { result.Crop(6, 0, 0, 0); };
+            Assert.ThrowsException<ArgumentException>(action);
         }
     }
 }
